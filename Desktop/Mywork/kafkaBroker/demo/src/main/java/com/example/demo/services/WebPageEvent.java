62c850687374eb5b8bd81c09b0aff4619fd3257e
OussamaBelkadi <OussamaYC@hotmail.com>
2024-06-30 14:46:07 +0000

    exemple producer poller

--- Desktop/Mywork/kafkaBroker/demo/src/main/java/com/example/demo/services/WebPageEvent.java
+++ Desktop/Mywork/kafkaBroker/demo/src/main/java/com/example/demo/services/WebPageEvent.java
@@ -4,7 +4,9 @@ import com.example.demo.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Service
 public class WebPageEvent {
@@ -15,5 +17,10 @@ public class WebPageEvent {
             System.out.println(input.toString());
             System.out.println("*************************");
         };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->new PageEvent("os","U1",new Date());
     }
 }
